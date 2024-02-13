d30fa4b01fe43630f335f5b3c27a101484b35fd9
ramirezDg <ramirezgil.daniel27@gmail.com>
2024-02-13 14:24:55 -0500

    Add: Normalize.css

--- ramirezDg-app/tailwind.config.js
+++ ramirezDg-app/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ['Inter", sans-serif;']
+      }
+    },
   },
   plugins: [],
 }
